11364688523a655de31207761b9fcea5674e27cf
iamkellly3 <142202403+iamkellly3@users.noreply.github.com>
2023-08-16 00:44:20 -0700

    Update first_streamlit_app.py
    Header "Fruityvice Fruit Advice!"

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -26,3 +26,6 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+#adding new header#
+streamlit.header("Fruityvice Fruit Advice!")
+
