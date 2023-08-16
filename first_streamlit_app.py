054751caad826b4484ea5e62630ec158f7cc5265
iamkellly3 <142202403+iamkellly3@users.noreply.github.com>
2023-08-16 00:49:23 -0700

    Update first_streamlit_app.py
    kiwi

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #new section to display fruityvice api response#
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 
 #take the json version of the response and noramlize it
